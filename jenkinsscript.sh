00eb42bc539535c62fdfe90f8a74af3716914032
PatelMinal <43324832+PatelMinal@users.noreply.github.com>
2019-01-09 09:30:32 +0000

    Update jenkinsscript.sh

--- jenkinsscript.sh
+++ jenkinsscript.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+sudo apt upadte 
+sudo apt install openjdk-8-jdk
+
 wget -q -O -https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add - 
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ < /etc/apt/sources.list.d/jenkins.list'
 
