7c9236af712c17e7002226e0d5f3f992d09eaa0c
PatelMinal <43324832+PatelMinal@users.noreply.github.com>
2019-01-09 09:21:21 +0000

    Add files via upload

---
+++ jenkinsscript.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+wget -q -O -https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key 
+add - 
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ < 
+/etc/apt/sources.list.d/jenkins.list'
+
+sudo apt update 
+sudo apt install jenkins -y
+sudo systemct1 staus jenkins
+curl localhost:8080
